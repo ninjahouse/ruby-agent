98c5435e043415403e150f3e576ffb25a4dddeb6
Fernando Papito <eu@papito.io>
2018-07-08 12:33:30 -0300

    add tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,11 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :selenium do |app|
+  options = { js_errors: false }
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 
 Capybara.configure do |c|
   c.default_driver = :selenium_chrome_headless
