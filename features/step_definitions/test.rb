b7532359735882bcdccf1d192ed9e1e1d8777376
Papito <eu@papito.io>
2018-07-25 18:14:55 -0300

    Update test.rb

--- features/step_definitions/test.rb
+++ features/step_definitions/test.rb
@@ -1,3 +1,5 @@
+require 'mongo'
+
 Given('context') do
   visit 'http://mark7.herokuapp.com'
 end
@@ -8,4 +10,10 @@ end
 
 Then('outcome') do
   expect(page).to have_content 'Fazer Login no Mark7'
+    
+  srt_conn = 'mongodb://aluno:qaninja@ds225078.mlab.com:25078/heroku_4m3km28x'
+  client = Mongo::Client.new(srt_conn)
+
+  tasks = client[:tasks]
+  tasks.delete_many(title: 'Livro')
 end
