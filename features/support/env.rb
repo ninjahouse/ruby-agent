84ac5a7cd2f1a251f49038e7373c571214968e63
Fernando Papini <papini@dotz.com>
2018-07-26 11:26:08 -0300

    remove selenium

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,24 @@
 require 'capybara'
 require 'capybara/cucumber'
-# require 'capybara/poltergeist'
+require 'selenium-webdriver'
 
-# Capybara.register_driver :selenium do |app|
-#   options = { js_errors: false }
-#   Capybara::Poltergeist::Driver.new(app, options)
-# end
+Capybara.javascript_driver = :selenium
+Capybara.run_server = false
+
+# Configure the Chrome driver capabilities & register
+args = ['--no-default-browser-check', '--start-maximized']
+caps = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => { 'args' => args })
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: 'http://localhost:4444/wd/hub',
+    desired_capabilities: caps
+  )
+end
 
 Capybara.configure do |c|
-  c.default_driver = :selenium_chrome_headless
+  c.default_driver = :selenium
 end
 
 Capybara.default_max_wait_time = 10
