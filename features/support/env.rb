67dded57b2fe2c503710ea3e8a4b8ea478920425
Fernando Papini <papini@dotz.com>
2018-07-26 09:54:21 -0300

    chrome headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,14 @@
 require 'capybara'
 require 'capybara/cucumber'
-require 'capybara/poltergeist'
+# require 'capybara/poltergeist'
 
-Capybara.register_driver :selenium do |app|
-  options = { js_errors: false }
-  Capybara::Poltergeist::Driver.new(app, options)
-end
+# Capybara.register_driver :selenium do |app|
+#   options = { js_errors: false }
+#   Capybara::Poltergeist::Driver.new(app, options)
+# end
 
 Capybara.configure do |c|
-  c.default_driver = :selenium
+  c.default_driver = :selenium_chrome_headless
 end
+
+Capybara.default_max_wait_time = 10
